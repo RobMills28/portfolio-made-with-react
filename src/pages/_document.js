2ffeb49cd70282e3789db746b645f76399256515
RobMills28 <63130550+RobMills28@users.noreply.github.com>
2022-02-09 16:57:35 +0000

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,7 +33,7 @@ export default class MyDocument extends Document {
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
       <title>
-        Website Portfolio
+        Rob's Portfolio
       </title>
         </Head>
         <body>
